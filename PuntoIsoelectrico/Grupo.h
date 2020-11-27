f99cbd4c086606cdc9ea6da0714cf26f6778585c
brunolopez941 <brunolopez941@gmail.com>
2020-11-26 20:51:00 -0600

    Create Grupo.h

---
+++ PuntoIsoelectrico/Grupo.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Grupo {
+private:
+	string nombreGrupo;
+	float pkGrupo;
+	int cargaGrupo;
+public:
+	Grupo() { nombreGrupo = ""; pkGrupo = 0; cargaGrupo = 0; };
+	Grupo(string nom, float pk, int carga) { nombreGrupo = nom; pkGrupo = pk; cargaGrupo = carga; };
+	float GetPkGrupo();
+	int GetCargaGrupo();
+};
